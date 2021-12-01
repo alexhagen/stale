249a212f42c7422dcae954aa7553940ef1b6ce8e
Alex Hagen <alexander.hagen@pnnl.gov>
2021-12-01 09:36:09 -0800

    wrote basic method, will refactor when I understand context managers

---
+++ stale/__init__.py
@@ -0,0 +1,27 @@
+import os
+import datetime
+
+class StaleFile:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def save(self, contents, keep_len):
+        if isinstance(contents, str):
+            with open(self.filename, 'w') as f:
+                f.write(contents)
+        elif isinstance(contents, bytearray):
+            with open(self.filename, 'wb') as f:
+                f.write(contents)
+        now = datetime.datetime()
+        stale_time = now + keep_len
+        os.setxattr(self.filename, 'user.staletime', stale_time)
+
+    def open(self, typecode):
+        now = datetime.datetime()
+        stale_time = os.getxattr('foo.txt', 'user.bar')
+        if now > stale_time:
+            return None
+        else:
+            with open(self.filename, typecode) as f:
+                contents = f.read()
+            return contents 
